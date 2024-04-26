afeb38ed09aec009589abc451ffe7721a69e6b81
divyansh48 <166692812+divyansh48@users.noreply.github.com>
2024-04-26 11:11:34 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,20 @@
+name = "Divyansh"
+friend = "rahul"
+anotherfriend = "lovish"
+apple = """he said,
+how to do work in python 
+and make output visible on 
+terminal"""
+name1 = input("enter your name: ")
+# print("hello,", name1, "!!!")
+
+# print(name1[0])
+# print(name1[1])
+# print(name1[2])
+# print(name1[3])
+# print(name1[4])
+
+# print(name1[8]) this will give error 
+
+for i in name1:
+    print(i)
