a14384e364a8626458453ac1d73e60c307b5f51d
divyansh48 <166692812+divyansh48@users.noreply.github.com>
2024-04-27 09:12:49 +0530

    Add files via upload

---
+++ stringslicing.py
@@ -0,0 +1,13 @@
+name= "divyansh"
+
+print(name[:5])
+print(name[1:7])
+print(name[-6:])
+print(name[:])
+print(name[-8:-1])
+print(name[1:4:2])
+print(name[4::4])
+print(name[2:7:7])
+print(name[5:8:1])
+print(name[7::2])
+print(name[-1:-8:1])
