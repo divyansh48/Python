db5b2bfd8383429cc8dcd17ae62bad9a648455e2
divyansh48 <166692812+divyansh48@users.noreply.github.com>
2024-04-29 08:32:23 +0530

    Add files via upload

---
+++ stringmethod.py
@@ -0,0 +1,11 @@
+a = "Divyansh"
+
+print(len(a))
+print(a.capitalize())
+print(a.endswith("v"))
+print(a.rstrip(" "))
+print(a.replace("D","z"))
+print(a.count("i"))
+print(a.swapcase())
+print(a.upper())
+# there are much more
