1c6e164ee1a2499e47fae0f96c31963462f23de9
divyansh48 <166692812+divyansh48@users.noreply.github.com>
2024-05-03 10:57:51 +0530

    Add files via upload

---
+++ breakcontinue.py
@@ -0,0 +1,12 @@
+for i in range(12):
+    if(i==10):
+        break;
+    print("5 X", i , "=", (5*i))
+
+for i in range(12):
+    
+    if(i==10):
+        print("hello")
+        continue;
+    print("hi")
+    
