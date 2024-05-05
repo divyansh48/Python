326dbef2533947ae5764cf0dfae1428c3fd04b78
divyansh48 <166692812+divyansh48@users.noreply.github.com>
2024-05-05 08:51:26 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,13 @@
+def  calculategmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isgreater(a,b):
+    if(a>b):
+        print("first number is greater than second number")
+    else:
+        print("second number is greater or equal to first number")
+
+isgreater(5,10)
+calculategmean(5,9)
+
