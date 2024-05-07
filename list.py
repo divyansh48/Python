eef41b3a33a32d81fa934ef1792f1bc540f2e0b6
divyansh48 <166692812+divyansh48@users.noreply.github.com>
2024-05-07 16:43:54 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,19 @@
+marks = [2,3,5,"divyansh",True,False]
+
+print(marks[0])
+print(marks[1])
+print(marks[2])
+print(marks[3])
+print(marks[4])
+
+marks1 = [2,3,5,"divyansh",True,False,345,424,478,2,215,643]
+print(marks1[0:])
+print(marks1[5:9])
+print(marks1[-1:9])
+print(marks1[-7:-5])
+print(marks1[:-5])
+print(marks1[:])
+print(marks1[::2])
+print(marks1[0:9:8])
+print(marks1[-10:-1:2])
+
